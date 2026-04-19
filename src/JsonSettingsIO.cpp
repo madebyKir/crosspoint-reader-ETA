91e5bc1923b68093c0e82f136204ba79bd78c15a
madebyKir <melomah@mail.ru>
2026-04-19 16:07:44 +0300

    Fix RecentBooksActivity build error: replace missing STR_CLOSE

--- src/JsonSettingsIO.cpp
+++ src/JsonSettingsIO.cpp
@@ -321,6 +321,7 @@ bool JsonSettingsIO::saveRecentBooks(const RecentBooksStore& store, const char* 
     obj["title"] = book.title;
     obj["author"] = book.author;
     obj["coverBmpPath"] = book.coverBmpPath;
+    obj["isRead"] = book.isRead;
   }
 
   String json;
@@ -345,6 +346,7 @@ bool JsonSettingsIO::loadRecentBooks(RecentBooksStore& store, const char* json) 
     book.title = obj["title"] | std::string("");
     book.author = obj["author"] | std::string("");
     book.coverBmpPath = obj["coverBmpPath"] | std::string("");
+    book.isRead = obj["isRead"] | false;
     store.recentBooks.push_back(book);
   }
 
